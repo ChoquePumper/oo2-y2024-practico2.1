eb43de2a0e4a6ba95d8ba40cc0e6e400c4e24e64
Andrés Choque Lopez <andreschoquelopez@gmail.com>
2024-04-02 20:50:38 -0300

    Commit inicial.

---
+++ src/main/java/oo2/practico2d1/ejercicio1/AprendiendoLambdas.java
@@ -0,0 +1,29 @@
+package oo2.practico2d1.ejercicio1;
+
+interface A {
+	void metodo();
+}
+interface B {
+	void metodo(String b);
+}
+interface C {
+	boolean metodo(String c);
+}
+interface D<T, R> {
+	R metodo(T c);
+}
+
+public class AprendiendoLambdas {
+	public void unMetodo(A a) {
+		a.metodo();
+	}
+	public void unMetodo(B b) {
+		b.metodo("unString");
+	}
+	public void unMetodo(C c) {
+		System.out.println(c.metodo("otroString") ? "true" : "false");
+	}
+	public void unMetodo(D<Long, Long> d) {
+		d.metodo(10L);
+	}
+}
---
+++ src/main/java/oo2/practico2d1/ejercicio1/Main.java
@@ -0,0 +1,16 @@
+package oo2.practico2d1.ejercicio1;
+
+public class Main {
+	public static void main(String[] args) {
+
+		AprendiendoLambdas a = new AprendiendoLambdas();
+		a.unMetodo((b) -> { System.out.println("abcd" + b);});	// B
+		a.unMetodo(() -> System.out.println("abcd"));	// A
+		a.unMetodo((variable) -> {System.out.println("abcd");});	// B
+		a.unMetodo((variable) -> {System.out.println("abcd"); return true;}); // C?
+		a.unMetodo((Long variable) -> {
+			System.out.println("abcd");
+			return 10L;
+		});	// D
+	}
+}
