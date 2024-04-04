a61ef1ddeef9d3657369bdaaa1ab36b1dc58830d
Andrés Choque Lopez <andreschoquelopez@gmail.com>
2024-04-04 19:44:04 -0300

    Ejercicio 1: agregar casting a (C) en el cuarto lambda debido a que se detectó una ambigüedad.

--- src/main/java/oo2/practico2d1/ejercicio1/Main.java
+++ src/main/java/oo2/practico2d1/ejercicio1/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		a.unMetodo((b) -> { System.out.println("abcd" + b);});	// B
 		a.unMetodo(() -> System.out.println("abcd"));	// A
 		a.unMetodo((variable) -> {System.out.println("abcd");});	// B
-		a.unMetodo((variable) -> {System.out.println("abcd"); return true;}); // C?
+		a.unMetodo((C) (variable) -> {System.out.println("abcd"); return true;}); // C?
 		a.unMetodo((Long variable) -> {
 			System.out.println("abcd");
 			return 10L;
